374042e3a593705c65ce6824536f52513edf123c
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2023-12-16 10:01:14 +0100

    created project, environments and assets folder

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+
+  //https://unbreakable-c84121382481.herokuapp.com/
+  API_URL: 'http://localhost:3031',
+  NATIONS_API: 'https://restcountries.com/v3.1/all',
+  CITIES_API: 'https://restcountries.com/v3.1/name/',
+  VERSION: '0.02',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+
+  //https://unbreakable-c84121382481.herokuapp.com/
+    API_URL: 'http://localhost:3031',
+    VERSION: '0.01'
+  };
