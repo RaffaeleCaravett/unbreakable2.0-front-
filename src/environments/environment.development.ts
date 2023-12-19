da64f8a59c1cc2cb383e75987fc6a60e54aa56b6
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2023-12-19 20:51:15 +0100

    deployng trial version

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
 
   //https://unbreakable-c84121382481.herokuapp.com/
-  API_URL: 'http://localhost:3031',
-  //  API_URL:  'https://unbreakableraffelecaravetta-a4dafb21c669.herokuapp.com',
+  // API_URL: 'http://localhost:3031',
+   API_URL:  'https://unbreakableraffelecaravetta-a4dafb21c669.herokuapp.com',
   NATIONS_API: 'https://restcountries.com/v3.1/all',
   CITIES_API: 'https://restcountries.com/v3.1/name/',
   VERSION: '0.02',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
 
   //https://unbreakable-c84121382481.herokuapp.com/
-   API_URL: 'http://localhost:3031',
-    // API_URL:  'https://unbreakableraffelecaravetta-a4dafb21c669.herokuapp.com',
+  //  API_URL: 'http://localhost:3031',
+    API_URL:  'https://unbreakableraffelecaravetta-a4dafb21c669.herokuapp.com',
     VERSION: '0.01'
   };
